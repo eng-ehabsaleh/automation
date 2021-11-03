61961ad864ff2f1a3f6e2cd7f382fe62d0644a14
ehab-ahma <ehab24495@gmail.com>
2021-11-03 22:33:21 +0200

    done

--- automation/automation.py
+++ automation/automation.py
@@ -1,2 +1,36 @@
+import re
+
+def numbers():
+
+    f = open("potential-contacts.txt", "r").read()
+    numbers = re.findall(r'(\d\d\d-\d\d\d-\d\d\d\d)', f)
+    number_content = ''
+    for num in sorted(numbers):
+        number_content += str(num)
+        number_content += f'\n'
+
+    with open('phone_numbers.txt', 'w+') as file:
+        file.write(number_content)
+    return number_content
+		
+
+
+def email():
+    f = open("potential-contacts.txt", "r").read()
+    email = re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', f)
+    email_content = ''
+    for em in sorted(email):
+        email_content += str(em)
+        email_content += f'\n'
+
+    with open('emails.txt', 'w+') as file:
+        file.write(email_content)
+        return email_content
+
+
+print(email())
+print(numbers())
+
+
 
 
--- tests/test_automation.py
+++ tests/test_automation.py
@@ -1,5 +1,13 @@
-from automation import __version__
+from automation.automation import numbers,email
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_numbers():
+    actual = numbers()
+    excepted = open("phone_numbers.txt", "r").read()
+    assert actual == excepted
+
+
+def test_email():
+    actual = email()
+    excepted = open("emails.txt", "r").read()
+    assert actual == excepted
